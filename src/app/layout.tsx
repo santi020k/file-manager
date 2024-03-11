f4c3e800dbd6e2b95e57bf65098cb4dd8b65e40d
Santiago Molina Orozco <santi020k@gmail.com>
2024-03-11 18:36:25 -0300

    Add Favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,17 @@ export default function RootLayout ({
 }>) {
   return (
     <html lang="en">
+      {/* TODO: Temporal Location */}
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+      <link rel="manifest" href="/favicons/site.webmanifest" />
+      <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#31363f" />
+      <link rel="shortcut icon" href="/favicons/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#31363f" />
+      <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
+      <meta name="theme-color" content="#31363f" />
+
       <body className={`${poppins.variable} prose max-w-none font-sans antialiased`}>{children}</body>
     </html>
   )
